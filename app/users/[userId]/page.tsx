008dbc2a916f0684840a7c48689fff9642f3dd99
bappyasif <bappyasif@hotmail.com>
2023-08-09 21:18:27 +0600

    isr, ssr, ssg, notfound
    * making use of those concepts using ts

--- app/users/[userId]/page.tsx
+++ app/users/[userId]/page.tsx
@@ -3,6 +3,8 @@ import { fetchUserPosts } from "@/libs/fetchUserPosts"
 import { Suspense } from "react"
 import UserPosts from "./components/UserPosts"
 import { Metadata } from "next"
+import { fetchUsers } from "@/libs/fetchUsers"
+import {notFound} from "next/navigation"
 
 type Params = {
     params: {
@@ -14,7 +16,25 @@ type Params = {
 export async function generateMetadata ({params: {userId}}: Params): Promise<Metadata> {
     // de duplicated user data will be fetched
     const userData: Promise<User> = fetchUser(userId)
-    const user:User = await userData
+    const user: User = await userData
+
+    // dynamic metadata when user is not found
+    if (!user || !user.name) {
+        // console.log("INSIDE METRE")
+        return {
+            title: "User Is Not Found"
+        }
+    }
+
+    // console.log(user, "USER!!")
+
+    // dynamic metadata when user is not found
+    // if(!user) {
+    //     console.log("running!!!!")
+    //     return {
+    //         title: "User Is Not Found!!"
+    //     }
+    // }
 
     return {
         title: user.name,
@@ -22,6 +42,7 @@ export async function generateMetadata ({params: {userId}}: Params): Promise<Met
     }
 
 }
+
 export default async function UserPage({ params: { userId } }: Params) {
     const userData: Promise<User> = fetchUser(userId)
 
@@ -31,7 +52,15 @@ export default async function UserPage({ params: { userId } }: Params) {
     // but this can be improved upon by using docs 4th criteria, which is incrementally fetch and show data when ready
     // const [user, userPosts] = await Promise.all([userData, userPostsData])
 
-    const user = await userData
+    const user:User = await userData
+
+    // console.log(user, "USER DATA!!")
+
+    // if user is not found we will show notFound page provided by nextjs
+    if(!user || !user.name) {
+        // if we have aour own not found page defined in route folder then it will render that instead of what served by nextjs
+        return notFound()
+    }
 
     return (
         <>
@@ -44,10 +73,14 @@ export default async function UserPage({ params: { userId } }: Params) {
     )
 }
 
-// const UserPosts = ({...posts}: Post[]) => {
-//     const {} = posts
-//     return (
-//         <>
-//         </>   
-//     )
-// }
+// using generateStaticParams will tell next js in advance that what "dynamically" generated params will be in use so that it can fetches data in advance for those params
+// when we use this it becomes SSG and not SSR
+export async function generateStaticParams() {
+    // in this case we want to generate our user pages in advance, we can do that by giving nextjs out dynamic user ids back to it so that it can fetches data in background for our dynamic user page rendering
+    const usersData: Promise<User[]> = fetchUsers()
+    const users = await usersData;
+
+    return users.map(user => ({
+        userId: user.id.toString()
+    }))
+}
--- libs/fetchUser.ts
+++ libs/fetchUser.ts
@@ -3,7 +3,9 @@ export const fetchUser = async (userId: string) => {
     const resp = await fetch(user_url)
 
     if (!resp.ok) {
-        throw new Error("failed to fetch user")
+        // as we will be using error page, lets return undefined instead of throwing an error
+        // throw new Error("failed to fetch user")
+        return undefined
     }
 
     return resp.json()
