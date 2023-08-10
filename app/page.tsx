9a87feca3a254650f77fe76ad7e75d8b946988ae
bappyasif <bappyasif@hotmail.com>
2023-08-11 00:26:49 +0600

    blog website with markdowns
    * making use of ssg, fetching, file systems, metadata, error page, dynamic routing

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+import Posts from "./components/Posts";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {/* <h1>hello world!!</h1>       */}
+    <main className="px-6 mx-auto">
+      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+        Hello and Welcome 👋&nbsp;
+        <span className="whitespace-nowrap">
+          I&apos;m <span className="font-bold">AB</span>.
+        </span>
+      </p>
+      <Posts />
     </main>
   )
 }
---
+++ app/posts/[postId]/not-found.tsx
@@ -0,0 +1,7 @@
+function NotFound() {
+  return (
+    <h1>This requested post is not found!!</h1>
+  )
+}
+
+export default NotFound
