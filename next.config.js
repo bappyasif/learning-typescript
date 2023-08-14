c22008aa539a66d6bc8f778c0d0ec194f84ac4aa
bappyasif <bappyasif@hotmail.com>
2023-08-15 01:29:07 +0600

    added image optimization techniques
    * used next image conig for remote locations
    * used sizes and lazy, priority for better image optimization performance

--- next.config.js
+++ next.config.js
@@ -8,7 +8,18 @@ const nextConfig = {
                 port: '',
                 pathname: '/u/**',
             },
+            // {
+            //     protocol: 'https',
+            //     hostname: 'images.unsplash.com',
+            //     port: '',
+            //     pathname: '/photo-**',
+            // }
+            // {
+            //     hostname: 'images.unsplash.com',
+            //     //in my case i used cdn.pixabay.com
+            // },
         ],
+        domains: ["images.unsplash.com", "source.unsplash.com"]
     },
 }
 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import Image from "next/image";
+
 export default async function Home() {
   return (
-    <h1 className="text-5xl">Public Home Page</h1>
+    <>
+      <h1 className="text-5xl">Public Home Page</h1>
+      <Image
+        src={"https://source.unsplash.com/random/beautiful"}
+        alt="a random picture from unsplash api"
+        width={650}
+        height={366}
+        // loading="lazy"
+        // sizes="(min-width: 808px) 50vw, 100vw"
+        sizes="(min-width: 1300px) 650px, (min-width: 820px) 49.35vw, (min-width: 760px) 650px, calc(93.18vw - 40px)"
+        priority={true}
+      />
+    </>
   )
 }
